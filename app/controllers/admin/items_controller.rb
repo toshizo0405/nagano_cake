3b2a33bf04e7606212bcb277bb95ebfde95473a9
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2022-12-21 09:43:13 +0000

    [Update17]課題・実装

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,26 +2,34 @@ class Admin::ItemsController < ApplicationController
   def index
     @items= Item.all
   end
-  
+
   def new
     @item = Item.new
   end
-  
+
   def create
      @item = Item.new(item_params)
   	 @item.save
   	 redirect_to admin_item_path(@item)
   end
-  
+
   def show
     @item = Item.find(params[:id])
   end
-  
+
+
   def edit
   end
-  
+
   def update
   end
-  
-  
+
+
 end
+
+
+ private
+
+  def item_params
+  	params.require(:item).permit(:name,:introduction,:price,:is_active,:item_image)
+  end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,11 @@
 class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_items, dependent: :destroy
-  
-  
-  
+
+   has_one_attached :image
+
+
   def add_tax_price
-        (self.price * 1.10).round
+    (price * 1.1).round
   end
 end
