af45100d45d0a09a16af34d173f74e815e40895f
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2022-12-22 12:42:28 +0000

    [Update22]課題・実装

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,5 @@
 class Admin::HomesController < ApplicationController
   def top
+    @orders = Order.all.order(id: :desc)
   end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,10 @@
 class Admin::OrdersController < ApplicationController
+
+  def show
+  end
+
+  def update
+  end
+
+
 end
