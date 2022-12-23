b72b90e3e6f677c306e46a6aa5db26b2496394c5
EC2 Default User <ec2-user@ip-172-31-15-36.ap-northeast-1.compute.internal>
2022-12-23 07:12:45 +0000

    [Update23]課題・実装

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,29 @@
 class Admin::OrdersController < ApplicationController
 
   def show
+    @order = Order.find(params[:id])
+  	@order_items = @order.order_items
+  	@order_details = @order.order_details
   end
 
   def update
+    @order = Order.find(params[:id])
+  	@order_items = @order.order_items
+  	@order.update(order_params)
+
+
+ 	if @order.order_status == "入金確認"
+	     @order_items.update_all(making_status: 1)
+	end
+  		 redirect_to  admin_order_path(@order)
   end
+
+  private
+
+  def order_params
+  	params.require(:order).permit(:order_status, :user_address)
+  end
+
 
 
 end
