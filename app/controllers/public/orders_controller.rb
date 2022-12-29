5314bfdd9860999e910e8870d01e0f90eea87fca
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2022-12-29 06:05:45 +0000

    [Update37]課題・実装

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,19 @@
+class Public::OrdersController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def confirm
+  end
+
+  def thanx
+  end
+end
