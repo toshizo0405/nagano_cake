e9530260707a561b7d789abcf79e624a97b84cdc
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2022-12-29 05:32:36 +0000

    [Update36]課題・実装

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,3 +1,6 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
+
+  enum production_status_method: { cannot_manufactured: 0, waiting_production: 1,under_manufacture: 2,completion_production: 3 }
+
 end
