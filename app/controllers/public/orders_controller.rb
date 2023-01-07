48e854516712314f9b3740e22fe6f6a461c1fad9
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-01-07 08:01:06 +0000

    [Update46]課題・実装

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -18,8 +18,28 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @cart_items= CartItem.all
     @order=Order.new(order_params)
-    binding.pry
+    if params[:address2] =="1"
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    elsif params[:address2] =="0"
+        @order.postal_code=current_customer.postal_code
+        @order.address=current_customer.address
+        @order.name=current_customer.last_name+current_customer.first_name
+    elsif params[:address2] =="2"
+         @order.postal_code=params[:order][:postal_code]
+         @order.name=params[:order][:name]
+         @order.address=params[:order][:address]
+
+
+    end
+
+
+
+
   end
 
   def thanx
