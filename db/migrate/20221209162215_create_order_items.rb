ac4aab567241bca176cd1bf42de62096c4f5a555
EC2 Default User <ec2-user@ip-172-31-15-36.ap-northeast-1.compute.internal>
2023-01-10 12:53:46 +0000

    [Update51]課題・実装

--- db/migrate/20221209162215_create_order_items.rb
+++ db/migrate/20221209162215_create_order_items.rb
@@ -1,7 +1,8 @@
 class CreateOrderItems < ActiveRecord::Migration[6.1]
   def change
     create_table :order_items do |t|
-      
+      t.integer :order_id
+      t.integer :item_id
       t.integer :quantity
       t.integer :production_status,default: "0"
       t.integer :price
