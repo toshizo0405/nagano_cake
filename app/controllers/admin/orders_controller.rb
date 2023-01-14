20b606e6d7c1b4c1621d26a46cca0817e4c15849
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-01-14 13:31:43 +0000

    [Update60]課題・実装

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,7 +3,7 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   	@order_items = @order.order_items
-  	@order_details = @order.order_details
+  	
   end
 
   def update
@@ -12,16 +12,21 @@ class Admin::OrdersController < ApplicationController
   	@order.update(order_params)
 
 
- 	if @order.order_status == "入金確認"
-	     @order_items.update_all(making_status: 1)
-	end
-  		 redirect_to  admin_order_path(@order)
   end
 
+
+
+
+
+
   private
+  
+  
+  
+  
 
   def order_params
-  	params.require(:order).permit(:order_status, :user_address)
+  	params.require(:order).permit(:order_status, :address)
   end
 
 
