ea250cac0f7e359fa305e8076d8790ee3a681a2d
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-01-20 13:20:28 +0000

    [Update74]課題・実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
-  
+
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_root_path
+    when Castomer
+      root_path
+    end
+  end
+
+
+  def after_sign_out_path_for(resource)
+    case resource
+    when Admin
+      new_admin_session_path
+    when Castomer
+      root_path
+    end
+  end
+
+
 end
