aeefc47115532c93c87b3ed7f314cf7c4bc03131
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-01-21 06:10:37 +0000

    [Update77]課題・実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,16 @@ class Item < ApplicationRecord
 
    has_one_attached :image
 
+   def get_item_image(width,height)
+    unless image.attached?
+     file_path=Rails.root.join('app/assets/images/default-image.jpg')
+     image.attach(io:File.open(file_path),filename: 'default-image.jpg',content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+   end
+
+
+
    validates :name,presence:true
    validates :introduction,presence:true
    validates :price,presence:true
