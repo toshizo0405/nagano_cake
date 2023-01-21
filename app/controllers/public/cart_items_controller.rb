484c9382721f2794ffeb64a67b05f06ca8afca5e
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-01-21 13:19:56 +0000

    [Update79]課題・実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,14 +6,19 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item= current_customer.cart_items.new(cart_item_params)
-    @cart_item.item_id=params[:cart_item][:item_id]
-    @cart_item.save
-    redirect_to cart_items_path
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      add_item= current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      add_item.amount+=params[:cart_item][:amount].to_i
+      add_item.save
+    else
+      @cart_item.save
+    end
+      redirect_to cart_items_path
   end
 
   def update
     @cart_item=CartItem.find(params[:id])
-    @cart_item.update(quantity: params[:cart_item][:quantity].to_i)
+    @cart_item.update(amount: params[:cart_item][:quantity].to_i)
     redirect_to cart_items_path
   end
 
