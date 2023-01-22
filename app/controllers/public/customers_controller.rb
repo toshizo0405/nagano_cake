7f0e49b5faeb00b91fb5c7e3a6a1083d9761fef2
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-01-22 12:47:45 +0000

    [Update82]課題・実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -9,10 +9,10 @@ class Public::CustomersController < ApplicationController
     @customer = current_customer
   end
 
-  def updete
-    @public = Public.find(params[:id])
-    @public.update(public_params)
-    redirect_to edit_customer_path(@public.id)
+  def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to customers_my_page_path
   end
 
 
@@ -28,4 +28,9 @@ class Public::CustomersController < ApplicationController
     redirect_to root_path
   end
 
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number,:email)
+  end
 end
